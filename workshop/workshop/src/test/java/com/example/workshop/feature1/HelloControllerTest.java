5e458a3eda08b6005c0c046b659c514f2f14e02f
up1 <somkiat.p@gmail.com>
2019-02-26 13:00:35 +0700

    First test controller

---
+++ workshop/workshop/src/test/java/com/example/workshop/feature1/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.workshop.feature1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void sayHello() {
+        HelloResponse response = restTemplate.getForObject(
+                       "/hello/test", HelloResponse.class);
+
+        // Validate
+        assertEquals("Hello test", response.getMessage());
+    }
+}
